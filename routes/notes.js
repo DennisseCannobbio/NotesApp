8459e76bfb2d39594a1f5dd04594e7ee890dce69
DennisseCannobbio <dennissecannobbio@gmail.com>
2021-06-26 15:22:55 -0400

    CRUD Finished

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,81 @@
 const express = require('express')
 const router = express.Router()
+const Note = require('../models/note')
 
-//
-router.get('/notes', (req, res) => {
-    res.send('Notas')
+//All notes index
+router.get('/notes', async (req, res) => {
+    //Buscamos todas las notas, las ordenamos descendiente
+    const notes = await Note.find().sort({ date: 'desc'})
+    //Renderizamos la página de todas las notas
+    res.render('notes/allNotes', { notes })
+})
+
+// GET del formulario para agregar notas
+router.get('/notes/add', (req, res) => {
+    //Renderizamos la vista newNote para agregar notas
+    res.render('notes/newNote')
+})
+
+// POST del formulario para agregar notas 
+router.post('/notes/newNote', async (req, res) => {
+    // Utilizamos los parametros de notes => title y description
+    const { title, description} = req.body
+    // Si hay errores
+    const errors = []
+    if(!title) {
+        errors.push({text: 'Por favor, escriba un título'})
+    }
+    if(!description) {
+        errors.push({ text: 'Por favor, escriba una descripción'})
+    }
+    // Si el arreglo de errores es mayor a 0, entonces si hay errores.
+    // Renreiza la página de nueva nota de nuevo y le pasa el error
+    if(errors.length > 0) {
+        res.render('notes/newNote', {
+            errors,
+            title,
+            description
+        })
+    } else {
+        // Si no existen errores entonces agrega una nueva nota
+        const newNote = new Note({ title, description})
+        // Guardamos la nota en la base de datos
+        await newNote.save()
+        // Agregamos el mensaje de flash => Que se agregó correctamente
+        req.flash('success_msg', '¡Nota agregada correctamente!')
+        // Redirigimos a todas las notas
+        res.redirect('/notes')
+    }
+})
+
+// Routes para editar notas
+router.get('/notes/edit/:id', async (req, res) => {
+    //Buscamos por id de la nota para editar
+    const note = await Note.findById(req.params.id)
+    // Si funciona bien entonces editará la nota
+    res.render('notes/editNote', { note })
+})
+
+// Routes PUT para editar notas
+router.put('/notes/editNote/:id', async (req, res) => {
+    // Utilizamos los parametros de notes => title y description
+    const { title, description} = req.body
+    // Buscamos por id y editamos la nota => le pasamos el id, el titulo y la descripcion
+    await Note.findByIdAndUpdate(req.params.id, { title, description })
+    // Enviamos un mensaje de éxito con flash
+    req.flash('success_msg', '¡Nota actualizada correctamente!')
+    // Redireccionamos a todas las notas 
+    res.redirect('/notes')
+})
+
+// Routes para eliminar
+router.delete('/notes/delete/:id', async (req, res) => {
+    // Buscamos la nota por id y eliminamos esa nota seleccionada
+    await Note.findByIdAndDelete(req.params.id)
+    // Mensaje satisfactorio de flash
+    req.flash('success_msg', '¡Nota eliminada correctamente!')
+    //Redirigimos a todas las notas
+    res.redirect('/notes')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path')
 const expHbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const flash = require('connect-flash')
 
 // Initializations
 const app = express()
@@ -11,7 +14,8 @@ require('./database')
 // Settings
 app.set('port', process.env.PORT || 5000)
 app.set('views', path.join(__dirname, 'views'))
-app.engine('.hbs', expHbs({ 
+app.engine('.hbs', expHbs({
+    handlebars: allowInsecurePrototypeAccess(handlebars),
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
@@ -27,18 +31,23 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }))
+app.use(flash())
 
 // Global Variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg')
+    res.locals.error_msg = req.flash('error_msg')
+    
+    next()
+})
 
 // Routes
 app.use(require('./routes/index'))
 app.use(require('./routes/notes'))
 app.use(require('./routes/users'))
 
-
-
 // Static Files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "/public")));
 
 // Run server
 app.listen(app.get('port'), () => {
